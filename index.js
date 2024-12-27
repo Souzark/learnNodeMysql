f42f8284eb07b0a9f08893b9aab0e04d72aaf4ed
Souzark <williamcoelhodesouza@gmail.com>
2024-12-27 19:45:42 -0300

    adicionando table com informações do usuário

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-const sequelize = require('sequelize');
-const sequelize = new Sequelize('test', 'root,'6cfcf526', {
-    host: 'localhost',
-    dialect: 'mysql'
+const Sequelize = require('sequelize');
+
+// Corrigido: parâmetros bem formatados
+const sequelize = new Sequelize('test', 'root', '6cfcf526', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+
+const Postagem = sequelize.define('postagens', {
+  titulo: {
+    type: Sequelize.STRING
+  },
+  conteudo: {
+    type: Sequelize.TEXT
+  }
+});
+
+const Usuario = sequelize.define('usuarios', {
+nome: {
+    type: Sequelize.STRING
+},
+sobrenome: {
+    type: Sequelize.STRING
+},
+idade: {
+    type: Sequelize.INTEGER
+},
+email: {
+    type: Sequelize.STRING
+}
+});
+
+Postagem.sync({ force: true })
+  .then(() => {
+    console.log('Tabela criada com sucesso!');
+  })
+  .catch((error) => {
+    console.error('Erro ao criar tabela:', error);
   });
 
-  const Postagem = sequelize.define('postagens', {
-    titulo: {
-      type: Sequelize.STRING
-    },
-    conteudo: {
-      type: Sequelize.TEXT
-    };
-  });
 
-  postagem.sync({force: true});
+Usuario.sync({ force: true }) // force: true força a criação da tabela
